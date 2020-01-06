2b92d0656b93e424ce51c0da8e2c2f91f9b95bc8
Andrew Ghostuhin <me@torinasakura.name>
2020-01-06 22:49:15 +0500

    chore: configure monorepo
    affects: @microfleet/app

---
+++ packages/app/test/app.js
@@ -0,0 +1,9 @@
+const MicrofleetApp = require('../src')
+
+describe('server', () => {
+  it('should be able to start', async () => {
+    const microfleetApp = new MicrofleetApp()
+    await microfleetApp.connect()
+    await microfleetApp.close()
+  })
+})
