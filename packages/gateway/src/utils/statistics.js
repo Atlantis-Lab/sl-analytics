e294849fa1e85bff00fccfa6b3449703c5f860e6
Andrew Ghostuhin <me@torinasakura.name>
2020-01-11 01:13:14 +0500

    refactor: remove unused files
    affects: @au/gateway

--- packages/gateway/src/utils/statistics.js
+++
@@ -1,6 +0,0 @@
-const { NotImplementedError } = require('common-errors')
-
-module.exports = function statistics(params) {
-  // @TODO: implement statistics
-  throw new NotImplementedError('Method is not yet implemented.')
-}
