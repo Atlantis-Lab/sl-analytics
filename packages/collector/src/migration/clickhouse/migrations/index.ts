e793993ca2478b7e9ffb0884ad95f1a396c50174
Andrew Ghostuhin <me@torinasakura.name>
2020-01-21 20:35:51 +0500

    refactor(collector): remove redundant migration
    affects: @au/collector

--- packages/collector/src/migration/clickhouse/migrations/index.ts
+++ packages/collector/src/migration/clickhouse/migrations/index.ts
@@ -1,4 +1,3 @@
-import { initDatasetV1 } from './1_initDataSet_v1'
 import { initDatasetV2 } from './2_initDataSet_v2'
 
-export const migrations = [initDatasetV1, initDatasetV2]
+export const migrations = [initDatasetV2]
